1c698b8fe77154c223952f8ab1858754250c3b70
Sta Rog <satach@o2.pl>
2023-03-24 23:40:24 +0100

    something went wrong

--- OOP/buldings.py
+++ OOP/buldings.py
@@ -10,13 +10,13 @@ class Building:
         return {'north-east': [self.point_x + self.x_length, self.point_y + self.y_length], 'south-east': [self.point_y, self.point_x + self.x_length], 'south-west': [self.point_y, self.point_x], 'north-west': [self.point_y + self.y_length, self.point_x]}
 
     def area(self):
-        raise NotImplementedError
+        return self.x_length* self.y_length
 
     def volume(self):
-        raise NotImplementedError
+        return self.x_length* self.y_length * self.height
 
     def __repr__(self):
-        raise NotImplementedError
+        return self.height
 
 
 if __name__ == '__main__':
@@ -26,7 +26,6 @@ if __name__ == '__main__':
 
     b = Building(1, 2, 2, 3)
     b2 = Building(1, 2, 2, 3, 5)
-    print(json_dict(b.corners()))
     assert json_dict(b.corners()) == {'north-east': [4, 4], 'south-east': [1, 4],
                                       'south-west': [1, 2], 'north-west': [4, 2]}, "Corners"
     assert b.area() == 6, "Area"
