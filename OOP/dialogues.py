d8b6affb4baedd42fd387be542ef808251c4f479
Sta Rog <satach@o2.pl>
2023-04-02 22:18:50 +0200

    v3

--- OOP/dialogues.py
+++ OOP/dialogues.py
@@ -4,10 +4,22 @@ class Chat:
     def __init__(self):
         pass
 
+    def connect_human(self, human):
+
+    def show_human_dialogue(self):
+
+    def show_robot_dialogue(self):
+        
+
 
 class Human:
     def __init__(self, name):
         self.name = name
+
+
+    def send(self, message):
+
+
 class Robot:
     def __init__(self, name):
         self.name = name   
