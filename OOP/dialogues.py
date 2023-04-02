9358eba37324588d304c7bc76f9bbc2fb4f0c917
Sta Rog <satach@o2.pl>
2023-04-02 23:17:36 +0200

    v6

--- OOP/dialogues.py
+++ OOP/dialogues.py
@@ -1,6 +1,4 @@
-class Chat:
-    
-    
+class Chat:   
     def __init__(self):
         connected = []
         messages = []
@@ -13,9 +11,10 @@ class Chat:
         self.connected.append(robot)
 
     def show_human_dialogue(self):
+        return self.messages
 
     def show_robot_dialogue(self):
-        
+        return self.messages
 
 
 class Human:
@@ -25,7 +24,7 @@ class Human:
 
 
     def send(self, message):
-        self.chat.messages.append(message)
+        self.chat.messages.append(self.name + "said:" + message)
 
 
 class Robot:
@@ -34,4 +33,25 @@ class Robot:
         self.chat = None
 
     def send(self, message):
-        self.chat.messages.append(message)
+        self.chat.messages.append(self.name + "said:" + message)
+
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+    chat = Chat()
+    karl = Human("Karl")
+    bot = Robot("R2D2")
+    chat.connect_human(karl)
+    chat.connect_robot(bot)
+    karl.send("Hi! What's new?")
+    bot.send("Hello, human. Could we speak later about it?")
+    assert (
+        chat.show_human_dialogue()
+        == """Karl said: Hi! What's new?
+R2D2 said: Hello, human. Could we speak later about it?"""
+    )
+    assert (
+        chat.show_robot_dialogue()
+        == """Karl said: 101111011111011
+R2D2 said: 10110111010111100111101110011101011010011011"""
+    )
