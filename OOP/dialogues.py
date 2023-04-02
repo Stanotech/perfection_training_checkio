c5ba4ba4bca8f1f333a2bcae13764a7411736f34
Sta Rog <satach@o2.pl>
2023-04-02 21:56:32 +0200

    v2

--- OOP/dialogues.py
+++ OOP/dialogues.py
@@ -2,4 +2,12 @@ class Chat:
     connected = []
 
     def __init__(self):
-        
+        pass
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+class Robot:
+    def __init__(self, name):
+        self.name = name   
