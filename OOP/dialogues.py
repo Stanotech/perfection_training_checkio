0f0a1f555684fed9e2616989dc85062236b76d1d
Sta Rog <satach@o2.pl>
2023-04-03 12:01:38 +0200

    finished

--- OOP/dialogues.py
+++ OOP/dialogues.py
@@ -1,36 +1,28 @@
+# description: https://py.checkio.org/en/mission/dialogues/
+
 class Chat:   
     def __init__(self):
-        self.connected = []
         self.messages = []
 
     def connect_human(self, human):
-        self.connected.append(human)
         human.chat = self
 
     def connect_robot(self, robot):
-        self.connected.append(robot)
         robot.chat = self
 
-    def show_human_dialogue(self):   
-           
+    def show_human_dialogue(self):              
         return "\n".join(who + mes for who, mes in (self.messages)) 
 
-    def show_robot_dialogue(self):
-        
+    def show_robot_dialogue(self):        
         return "\n".join((who + ''.join("0" if char in "aeiouAEIOU" else "1" for char in mes)) for who, mes in (self.messages))  
-
-
 class Human:
     def __init__(self, name):
         self.name = name
         self.chat = None
 
-
     def send(self, message):
-        self.chat.messages.append((self.name + " said: ", message))
+        self.chat.messages.append((self.name + " said: ", message))        
 
-
-        
 class Robot:
     def __init__(self, name):
         self.name = name   
